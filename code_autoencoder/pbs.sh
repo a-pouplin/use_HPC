717140f612039b5427aea89b6e2ea31a1e322a8c
Alison <alison.pouplin16@imperial.ac.uk>
2019-02-05 10:49:06 +0000

    last edits

--- code_autoencoder/pbs.sh
+++ code_autoencoder/pbs.sh
@@ -1,5 +1,5 @@
 #PBS -lwalltime=00:30:00
 #PBS -lselect=1:ncpus=1:mem=8gb:ngpus=1
 module load anaconda3
-conda activate v-pytorch
-python /rds/general/user/amp115/home/code/use_HPC/code_autoencoder/main.py 
+source activate v-pytorch
+python /rds/general/user/amp115/home/code/use_HPC/code_autoencoder/main.py
