91a8e0fbb39b61abf98a9ecf09c0bca091c7f12a
Alison <alison.pouplin16@imperial.ac.uk>
2019-02-05 10:28:47 +0000

    example PBS script

---
+++ code_autoencoder/pbs.sh
@@ -0,0 +1,5 @@
+PBS -l walltime=00:30:00 ## time with hours:minutes:seconds
+PBS -lselect=1:ncpus=1:mem=8gb:ngpus=1 # specify the gpu you need. the lower the memory requested, the higher the priority
+module load anaconda3  # load anaconda3 or 2.
+conda activate v-pytorch # activate your environment
+python /rds/general/user/amp115/home/code/use_HPC/code_autoencoder/main.py #here is the full path to your code (mine as example)
